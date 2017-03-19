0f9b7b386e3ec76ac9d5ff38811ab6eb59cc3c06
Ivan Gayton <ivan@controller.access.network>
2017-03-19 10:16:01 +0000

    messing with Kobo Toolbox setup

--- kobotoolbox_server_secure_setup.sh
+++ kobotoolbox_server_secure_setup.sh
@@ -4,10 +4,31 @@ echo please enter your domain name
 read domain_name
 
 # install docker-compose
+sudo apt -y update
+sudo apt -y upgrade
 
-git clone https://github.com/kobotoolbox/kobo-docker.git
+sudo apt install apt install apt-transport-https \
+     ca-certificates curl software-properties-common
 
-sudo apt install -y letsencrypt
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt -y update
+
+sudo apt install -y docker-ce docker-compose
+
+sudo usermod -aG docker $USER
+
+if ! type "letsencrypt"; then
+    echo installing letsencrypt
+    sudo apt install -y letsencrypt
+else echo letsencrypt seems to be already installed
+fi
 
 sudo letsencrypt certonly --standalone -d $domain_name -d www.$domain_name -d kf.$domain_name -d kc.$domain_name -d en.$domain_name
 
+git clone https://github.com/kobotoolbox/kobo-docker.git
